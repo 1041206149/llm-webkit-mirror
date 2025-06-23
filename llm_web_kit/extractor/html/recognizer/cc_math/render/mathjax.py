f63bd8f7d7a3cbfb3a41fc4aa636cac1fc3c7f33
Yanggq <1041206149@qq.com>
2025-06-23 17:27:49 +0800

    Revert "转换方法转移到common.py，命名修改，添加测试方法"
    This reverts commit 3402fe6707357c33c0aaffeac255bac9ac1101e4.

--- llm_web_kit/extractor/html/recognizer/cc_math/render/mathjax.py
+++ llm_web_kit/extractor/html/recognizer/cc_math/render/mathjax.py
@@ -1,8 +1,9 @@
 import re
 from typing import Any, Dict, List
 
-from llm_web_kit.extractor.html.recognizer.cc_math.common import (
-    MATHINSIGHT, MATHINSIGHT_convert_to_standard_latex)
+from llm_web_kit.extractor.html.recognizer.cc_math.common import MATHINSIGHT
+from llm_web_kit.extractor.html.recognizer.cc_math.config.mathsight.formula_conversion import \
+    convert_to_standard_latex
 from llm_web_kit.extractor.html.recognizer.cc_math.render.render import (
     BaseMathRender, MathRenderType)
 from llm_web_kit.libs.html_utils import HtmlElement, html_to_element
@@ -378,7 +379,7 @@ class MathJaxRender(BaseMathRender):
 
             # 限定MATHINSIGHT域名
             if MATHINSIGHT.DOMAIN in self.url:
-                formula = MATHINSIGHT_convert_to_standard_latex(formula)
+                formula = convert_to_standard_latex(formula)
             start_pos = match.start()
             end_pos = match.end()
 
@@ -449,7 +450,7 @@ class MathJaxRender(BaseMathRender):
             formula = f'\\begin{{{env_name}}}{formula}\\end{{{env_name}}}'
             # 将自定义LaTeX转换为标准LaTeX格式
             if MATHINSIGHT.DOMAIN in self.url:
-                formula = MATHINSIGHT_convert_to_standard_latex(formula)
+                formula = convert_to_standard_latex(formula)
 
             start_pos = match.start()
             end_pos = match.end()
